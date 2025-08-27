8d4671a30c73128ae8cd155f20ac0fb9c121a243
Tanisha9138 <152298980+Tanisha9138@users.noreply.github.com>
2025-08-27 18:16:55 +0530

    modify this with fetch and loader

--- frontend/src/components/miniComponents/PopularAuthors.jsx
+++ frontend/src/components/miniComponents/PopularAuthors.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
 
 const PopularAuthors = () => {
+  const [authors, setAuthors] = useState([]);
+  useEffect(() => {
+    const fetchAuthors = async () => {
+      const { data } = await axios.get(
+        "http://localhost:4000/api/v1/user/authors",
+        { withCredentials: true }
+      );
+      setAuthors(data.authors);
+    };
+    fetchAuthors();
+  }, []);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="popularAuthors">
+      <h3>Popular Authors</h3>
+      <div className="container">
+        {authors && authors.length > 0 ? (
+          authors.slice(0, 4).map((element) => {
+            return (
+              <div className="card" key={element._id}>
+                {/* <img src={element.avatar.url} alt="author" /> */}
+                <img src={element.avatar && element.avatar.url ? element.avatar.url : "/user.jpg"} alt="author" />
+                <p>{element.name}</p>
+                <p>{element.role}</p>
+              </div>
+            );
+          })
+        ) : (
+          <BeatLoader color="gray" size={30} />
+        )}
+      </div>
+    </section>
+  );
+};
 
-export default PopularAuthors
+export default PopularAuthors;
