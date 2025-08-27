a818db513cebf322007974012dc08e9007db1f5d
Tanisha9138 <152298980+Tanisha9138@users.noreply.github.com>
2025-08-27 18:22:31 +0530

    dashboard with sidebar, dynamic components, auth check

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React, { useContext, useState } from "react";
+import SideBar from "../layout/SideBar";
+import MyBlogs from "../miniComponents/MyBlogs";
+import MyProfile from "../miniComponents/MyProfile";
+import CreateBlog from "../miniComponents/CreateBlog";
+import Chart from "../miniComponents/Chart";
+import { Context } from "../../main";
+import { Navigate } from "react-router-dom";
 const Dashboard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [component, setComponent] = useState("MyBlogs");
+  const { mode, isAuthenticated } = useContext(Context);
 
-export default Dashboard
+  if (!isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <section
+      className={mode === "dark" ? "dark-bg dashboard" : "light-bg dashboard"}
+    >
+      <SideBar component={component} setComponent={setComponent} />
+      {component === "My Profile" ? (
+        <MyProfile />
+      ) : component === "Create Blog" ? (
+        <CreateBlog />
+      ) : component === "Analytics" ? (
+        <Chart />
+      ) : (
+        <MyBlogs />
+      )}
+    </section>
+  );
+};
+
+export default Dashboard;
