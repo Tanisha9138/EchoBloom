1e8b4a6c023274970cb0872a6bf866835393c2ce
Tanisha9138 <152298980+Tanisha9138@users.noreply.github.com>
2025-08-27 18:25:27 +0530

    home page with bloga and authors

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,19 +1,22 @@
-import React, {useContext} from 'react';
-import {Context} from "../../main";
+import React, { useContext, useState } from "react";
+import LatestBlogs from "../miniComponents/LatestBlog";
 import HeroSection from "../miniComponents/HeroSection";
 import TrendingBlogs from "../miniComponents/TrendingBlogs";
-import LatestBlog from "../miniComponents/LatestBlog";
 import PopularAuthors from "../miniComponents/PopularAuthors";
+import { Context } from "../../main";
 
 const Home = () => {
-  const {mode} = useContext(Context);
+  const { mode, blogs } = useContext(Context);
+  const filteredBlogs = blogs.slice(0, 6);
   return (
-    <article className={mode === "dark" ? "dark-bg" : "light-bg"}>
-      <HeroSection/>
-      <TrendingBlogs/>
-      <LatestBlog/>
-      <PopularAuthors/>
-    </article>
+    <>
+      <article className={mode === "dark" ? "dark-bg" : "light-bg"}>
+        <HeroSection />
+        <TrendingBlogs />
+        <LatestBlogs heading={"Latest Blogs"} blogs={filteredBlogs} />
+        <PopularAuthors />
+      </article>
+    </>
   );
 };
 
