0612d064a7381fec095285fb20ee677a7455ba16
Tanisha9138 <152298980+Tanisha9138@users.noreply.github.com>
2025-08-27 18:15:29 +0530

    add myprofile component to display user details

--- frontend/src/components/miniComponents/MyProfile.jsx
+++ frontend/src/components/miniComponents/MyProfile.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Context } from "../../main";
 
 const MyProfile = () => {
+  const { user } = useContext(Context);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="profile">
+      <div className="avatar">
+        <img src="/pic.jpg" alt="avatar" />
+      </div>
+      <div className="user-detail">
+        <p>
+          Name: <span>{user.name}</span>
+        </p>
+        <p>
+          Email: <span>{user.email}</span>
+        </p>
+        <p>
+          Phone: <span>{user.phone}</span>
+        </p>
+        <p>
+          Role: <span>{user.role}</span>
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default MyProfile
+export default MyProfile;
