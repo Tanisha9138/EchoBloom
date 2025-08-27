54cfc6ceb0546f55f904f007ac2c7aef54e2d1ab
Tanisha9138 <152298980+Tanisha9138@users.noreply.github.com>
2025-08-27 17:56:54 +0530

    define blog model

--- backend/models/blogSchema.js
+++ backend/models/blogSchema.js
@@ -1,97 +1,88 @@
 import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema({
-    title:{
-        type: String,
-        required: true,
-        minLength: [10, "Blog title must contain at least 10 characters!"],
-        maxLength: [40, "Blog title cannot exceed 40 characters!"],
+  title: {
+    type: String,
+    required: true,
+  },
+  mainImage: {
+    public_id: {
+      type: String,
+      required: true,
     },
-    mainImage:{
-        public_id:{
-            type: String,
-            required: true,
-        },
-        url:{
-            type: String,
-            required: true,
-        },
+    url: {
+      type: String,
+      required: true,
     },
-    intro:{
-        type: String,
-        required: true,
-        minLength: [250, "Blog intro must contain at least 250 characters!"],
+  },
+  intro: {
+    type: String,
+    required: true,
+    minLength: [250, "Blog intro must contain at least 250 characters!"],
+  },
+  paraOneImage: {
+    public_id: {
+      type: String,
     },
-    paraOneImage:{
-        public_id:{
-            type: String,
-        },
-        url:{
-            type: String,
-        },
+    url: {
+      type: String,
     },
-    paraOneDescription:{
-        type: String,
-        minLength: [50, "Blog intro must contain at least 50 characters!"],
+  },
+  paraOneDescription: {
+    type: String,
+  },
+  paraOneTitle: {
+    type: String,
+  },
+  paraTwoImage: {
+    public_id: {
+      type: String,
     },
-    paraOneTitle:{
-        type: String,
-        minLength: [50, "Blog title must contain at least 50 characters!"],
+    url: {
+      type: String,
     },
-    paraTwoImage:{
-        public_id:{
-            type: String,
-        },
-        url:{
-            type: String,
-        },
+  },
+  paraTwoDescription: {
+    type: String,
+  },
+  paraTwoTitle: {
+    type: String,
+  },
+  paraThreeImage: {
+    public_id: {
+      type: String,
     },
-    paraTwoDescription:{
-        type: String,
-        minLength: [50, "Blog intro must contain at least 50 characters!"],
+    url: {
+      type: String,
     },
-    paraTwoTitle:{
-        type: String,
-        minLength: [50, "Blog title must contain at least 50 characters!"],
-    },
-    paraThreeImage:{
-        public_id:{
-            type: String,
-        },
-        url:{
-            type: String,
-        },
-    },
-    paraThreeDescription:{
-        type: String,
-        minLength: [50, "Blog intro must contain at least 50 characters!"],
-    },
-    paraThreeTitle:{
-        type: String,
-        minLength: [50, "Blog title must contain at least 50 characters!"],
-    },
-    category:{
-        type: String,
-        required: true,
-    },
-    createdBy:{
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    authorName:{
-        type: String,
-        required: true,
-    },
-    authorAvatar:{
-        type: String,
-        required: true,
-    },
-    published: {
-        type: Boolean,
-        default: false,
-    },
+  },
+  paraThreeDescription: {
+    type: String,
+  },
+  paraThreeTitle: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  authorName: {
+    type: String,
+    required: true,
+  },
+  authorAvatar: {
+    type: String,
+    required: true,
+  },
+  published: {
+    type: Boolean,
+    default: false,
+  },
 });
-
 
 export const Blog = mongoose.model("Blog", blogSchema);
