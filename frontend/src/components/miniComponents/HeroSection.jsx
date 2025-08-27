c87229f2a11a50d047c42b1314f6035cc87b81cb
Tanisha9138 <152298980+Tanisha9138@users.noreply.github.com>
2025-08-27 19:34:55 +0530

    done

--- frontend/src/components/miniComponents/HeroSection.jsx
+++ frontend/src/components/miniComponents/HeroSection.jsx
@@ -1,31 +1,32 @@
-import React, {useContext} from 'react';
-import {Context} from "../../main";
-import { Link } from 'react-router-dom';
-import {BeatLoader} from 'react-spinners';
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { BeatLoader } from "react-spinners";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
-  const {blogs} = useContext(Context);
+  const { blogs } = useContext(Context);
   return (
-  <section className='hero'>
-    {
-      blogs && blogs.length > 0 ? (blogs.slice(0,2).map(element=>{
-        return(
-          <Link to={`/blog/${element._id}`} className='card' key={element._id}>
-            <img src={element.mainImage.url} alt='blog' className='blogImg'/>
-            <h1>{element.title}</h1>
-            <div className='writer_section'>
-              <div className="author">
-                <img src={element.authorAvatar} alt={"authorAvatar"}/>
-                <p>{element.authorName}</p>
+    <section className="hero">
+      {blogs && blogs.length > 0 ? (
+        blogs.slice(0, 2).map((element) => {
+          return (
+            <Link to={`/blog/${element._id}`} className="card" key={element._id}>
+              <img src={element.mainImage.url} alt="blog" className="blogImg" />
+              <div className="category">{element.category}</div>
+              <h1>{element.title}</h1>
+              <div className="writer_section">
+                <div className="author">
+                  <img src={element.authorAvatar} alt="author_avatar" />
+                  <p>{element.authorName}</p>
+                </div>
               </div>
-            </div>
-          </Link>
-        )
-      })) : (
-        <BeatLoader size={30} color='gray'/>
-      )
-    }
-  </section>
+            </Link>
+          );
+        })
+      ) : (
+        <BeatLoader color="gray" size={30} />
+      )}
+    </section>
   );
 };
 
--- frontend/src/components/miniComponents/PopularAuthors.jsx
+++ frontend/src/components/miniComponents/PopularAuthors.jsx
@@ -4,31 +4,37 @@ import { BeatLoader } from "react-spinners";
 
 const PopularAuthors = () => {
   const [authors, setAuthors] = useState([]);
+
   useEffect(() => {
     const fetchAuthors = async () => {
-      const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/authors",
-        { withCredentials: true }
-      );
-      setAuthors(data.authors);
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/user/authors",
+          { withCredentials: true }
+        );
+        setAuthors(data.authors);
+      } catch (error) {
+        console.error("Error fetching authors:", error);
+      }
     };
     fetchAuthors();
   }, []);
+
   return (
     <section className="popularAuthors">
       <h3>Popular Authors</h3>
       <div className="container">
         {authors && authors.length > 0 ? (
-          authors.slice(0, 4).map((element) => {
-            return (
-              <div className="card" key={element._id}>
-                {/* <img src={element.avatar.url} alt="author" /> */}
-                <img src={element.avatar && element.avatar.url ? element.avatar.url : "/user.jpg"} alt="author" />
-                <p>{element.name}</p>
-                <p>{element.role}</p>
-              </div>
-            );
-          })
+          authors.slice(0, 4).map((element) => (
+            <div className="card" key={element._id}>
+              <img
+                src={element.avatar && element.avatar.url ? element.avatar.url : "/user.jpg"}
+                alt="author"
+              />
+              <p>{element.name}</p>
+              <p>{element.role}</p>
+            </div>
+          ))
         ) : (
           <BeatLoader color="gray" size={30} />
         )}
