ce2e13b23d551eae87657521d21594c21d9040cd
Tanisha9138 <152298980+Tanisha9138@users.noreply.github.com>
2025-12-24 17:44:37 +0530

    Update API endpoint for fetching authors

--- frontend/src/components/miniComponents/PopularAuthors.jsx
+++ frontend/src/components/miniComponents/PopularAuthors.jsx
@@ -9,7 +9,7 @@ const PopularAuthors = () => {
     const fetchAuthors = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/user/authors",
+          "https://echobloom-backend.onrender.com/api/v1/user/authors",
           { withCredentials: true }
         );
         setAuthors(data.authors);
